3f0eae2adc0b4f2f0808752ad6201388f550347b
izadoramatias <contato.izadoramatias@gmail.com>
2021-07-08 22:40:07 -0300

    add cards, scrollreveal, slider, services and contacts

--- main.js
+++ main.js
@@ -30,3 +30,30 @@ window.addEventListener('scroll', function() {
     header.classList.remove('scroll')
   }
 })
+
+// testimonials carousel/slider/swiper
+
+const swiper = new Swiper('.swiper-container', {
+ slidesPerView: 1,
+ pagination: {
+   el: '.swiper-pagination'
+ },
+ mousewheel: true,
+ keyboard: true
+})
+
+// ScrollReveal: revela os elementos conforme o usuário dá um scroll na pag
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+scrollReveal.reveal(
+  `#home .image, #home .text, #home .text .button,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links`
+  
+  , { interval: 100})
